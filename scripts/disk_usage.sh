ab3638927bc24b84d843371d717dd2418474203d
Maytea <mitir79@gmail.com>
2026-08-30 21:25:22 +0330

    feat: add disk usage script

---
+++ scripts/disk_usage.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+read TOTAL USED AVAILABLE USAGE <<< "$(df -h / | awk '$6=="/" {print $2, $3, $4, $5}')"
+
+echo "=== Disk Usage ==="
+
+echo "Mount point: /"
+echo "Total: $TOTAL"
+echo "Used: $USED"
+echo "Available: $AVAILABLE"
+echo "Usage: $USAGE"
